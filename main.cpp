175729641dcf422aee03d9ebbcf0dd6ee2b66d60
Kiselev Vladislav <kiselev.vladislav@urfu.me>
2021-12-24 11:41:07 +0500

    Add a commentaries and error`s handler

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 char Dictionary[10000][100];
 
 int main() {
-    setlocale(LC_ALL, "rus");
-    int user_integer = 0;
+    setlocale(LC_ALL, "rus"); // Устанавливается использование русской локали
+    int user_integer = 0; // Инициализуем переменную для числа вводимого пользователем
     fstream file;
-    file.open("C:\\Users\\kisel\\CLionProjects\\Pluralize\\cmake-build-debug\\PluralizeDictionary.txt");
-    if (file.is_open()) {
-        for (int i = 0; !file.eof(); i++) {
+    file.open("C:\\POS-Manager\\APPLIED-PROGRAMMING-Pluralize-task\\PluralizeDictionary.txt"); // Открываем поток чтения из файла со словарём плюрализованных значений
+    if (file.is_open()) { // В случае успешного открытия файла - продолжаем выполнение программы
+        for (int i = 0; !file.eof(); i++) { // Считываем словарь
             file.getline(Dictionary[i], 100);
         }
         cout << "Введите число для плюрализации: ";
-        cin >> user_integer;
-        cout << "Ваше плюрализованное число: " << Dictionary[user_integer] << endl;
+        cin >> user_integer; // Пользователь вводит число для плюрализации
+        if (user_integer == 0) { // Проверяем на ошибки ввода
+            cout << "Вы ввели что-то отличное от положительного целого числа";
+        } else {
+            cout << "Ваше плюрализованное число: " << Dictionary[user_integer] << endl;
+        }
     }
     else {
         cout << "Dictionary reading error" << endl;
